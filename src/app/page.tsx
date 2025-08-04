05a7884fa071db1dd063472db71a3d0d58ca5187
Carlos Eduardo <cadumouralemes@gmail.com>
2025-08-04 16:16:41 -0300

    feat: adding page container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Container } from '@/components/Container/intex';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import { postRepository } from '@/repositories/post';
@@ -5,9 +6,28 @@ import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <div>
+    <Container>
       <header>
         <h1 className='text-6xl font-bold text-center py-8'>Header</h1>
+        <p className='text-justify'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+          eveniet natus optio reprehenderit, necessitatibus mollitia sequi
+          tempore nulla maxime dignissimos itaque obcaecati at et ducimus fuga
+          nemo commodi cum? Officiis, ad quibusdam? Deleniti dolorum perferendis
+          laboriosam saepe aperiam in minus beatae odio consequuntur nostrum
+          natus eveniet consequatur maiores accusamus aliquid, a assumenda
+          adipisci. Consectetur aspernatur enim at, ut suscipit quibusdam beatae
+          nobis totam iste autem temporibus! Ratione qui iure velit distinctio,
+          provident voluptate non voluptatum corporis quam odio culpa? Nostrum
+          commodi similique autem nulla architecto molestiae ducimus facere! Nam
+          labore dolorum dolor fuga, eaque quod ipsum aperiam tempore ad dolore
+          odio, officia voluptate delectus, laboriosam nihil aliquid consequatur
+          corporis cum! Soluta est perspiciatis nisi sequi id autem voluptates?
+          Repellendus, rem provident. Repellendus consequuntur voluptatem sequi
+          libero recusandae iusto repellat dolorem. Quae exercitationem aliquam
+          illum. Quod earum molestiae exercitationem est et quae ex sapiente eum
+          quasi cumque tenetur, reiciendis nihil natus, impedit quos ipsum
+        </p>
       </header>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
@@ -15,6 +35,6 @@ export default async function HomePage() {
       <footer>
         <h1 className='text-6xl font-bold text-center py-8'>footer</h1>
       </footer>
-    </div>
+    </Container>
   );
 }
