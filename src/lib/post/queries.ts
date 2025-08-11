9747f616466e417afb0f47936e790262293db6b0
Carlos Eduardo <cadumouralemes@gmail.com>
2025-08-11 16:31:45 -0300

    feat: improving findPostBySlugCached

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -1,13 +1,23 @@
 import { postRepository } from '@/repositories/post';
+import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
 export const findAllPublicPosts = cache(
   async () => await postRepository.findAllPublic(),
 );
 
-export const findPostBySlugCached = cache(
-  async (slug: string) => await postRepository.findBySlug(slug),
-);
+export const findPostBySlugCached = cache(async (slug: string) => {
+  let post;
+
+  try {
+    post = await postRepository.findBySlug(slug);
+  } catch {
+    post = undefined;
+  }
+
+  if (!post) notFound();
+  return post;
+});
 
 export const findByIdCached = cache(
   async (id: string) => await postRepository.findById(id),
