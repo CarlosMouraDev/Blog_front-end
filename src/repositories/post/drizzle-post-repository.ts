c613f3d122c67da834fb8543f3eb4eeb965d4816
Carlos Eduardo <cadumouralemes@gmail.com>
2025-08-12 19:37:42 -0300

    feat: adding findBySlugPublic query

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -13,7 +13,20 @@ export class DrizzlePostRepository implements PostRepository {
 
     return posts;
   }
+
+  async findBySlugPublic(slug: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      orderBy: desc(postsTable.createdAt),
+      where: (posts, { eq, and }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
+
+    if (!post) throw new Error('Post não encontrado');
+
+    return post;
+  }
+
   async findAll(): Promise<PostModel[]>;
+
   async findById(id: string): Promise<PostModel>;
-  async findBySlugPublic(slug: string): Promise<PostModel>;
 }
