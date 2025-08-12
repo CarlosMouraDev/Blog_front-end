388c560d57c93ce6f13cafc8e9e9c0a191139633
Carlos Eduardo <cadumouralemes@gmail.com>
2025-08-12 14:44:18 -0300

    feat: catching generic erros

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+import ErrorMessage from '@/components/ErrorMessage';
+import { useEffect } from 'react';
+
+type RootErrorPageProps = {
+  error: Error;
+  reset: () => void;
+};
+
+export default function RootErrorPage({ error, reset }: RootErrorPageProps) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <ErrorMessage
+      pageTitle='Internal Server Error'
+      contentTitle='501'
+      content='Ocorreu um erro inesperado, tente novamente mais tarde.'
+    />
+  );
+}
