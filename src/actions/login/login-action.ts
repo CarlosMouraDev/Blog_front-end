2fcaae09251d8766f8b4690ce95724b9fec14e42
Carlos Eduardo <cadumouralemes@gmail.com>
2025-09-01 17:53:57 -0300

    feat: generate hashing with bcrypt and verifying on login page

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,6 +1,8 @@
 'use server';
 
+import { verifyPassword } from '@/lib/login/manage-login';
 import { asyncDelay } from '@/utils/async-delay';
+import { error } from 'console';
 
 type LoginActionState = {
   username: string;
@@ -10,8 +12,34 @@ type LoginActionState = {
 export async function loginAction(state: LoginActionState, formData: FormData) {
   await asyncDelay(5000);
 
-  return {
-    username: '',
-    error: 'Teste de erro',
-  };
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Dados inválidos',
+    };
+  }
+
+  // User Data
+  const username = formData.get('username')?.toString().trim() || '';
+  const password = formData.get('password')?.toString().trim() || '';
+
+  if (!username || !password) {
+    return {
+      username: '',
+      password: '',
+    };
+  }
+
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = await verifyPassword(
+    password,
+    process.env.LOGIN_PASS || '',
+  );
+
+  if (!isUsernameValid || !isPasswordValid) {
+    return {
+      username,
+      error: 'Usuário ou senha inválidos.',
+    };
+  }
 }
