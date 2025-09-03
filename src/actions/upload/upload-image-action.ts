ed3c4c951bbc475cba13bb83e4b71e3aedcd96e9
Carlos Eduardo <cadumouralemes@gmail.com>
2025-09-02 21:37:08 -0300

    feat: adding auth checking on server actions

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { verifyLoginSession } from '@/lib/login/manage-login';
 import { mkdir, writeFile } from 'fs/promises';
 import { extname, resolve } from 'path';
 
@@ -11,7 +12,13 @@ type UploadImageActionResult = {
 export async function uploadImageAction(
   formData: FormData,
 ): Promise<UploadImageActionResult> {
+  const isAuthenticated = await verifyLoginSession();
+
   const makeResult = ({ url = '', error = '' }) => ({ url, error });
+
+  if (!isAuthenticated) {
+    return makeResult({ error: 'Faça login novamente em outra aba' });
+  }
 
   if (!(formData instanceof FormData)) {
     return makeResult({ error: 'Dados inválidos' });
