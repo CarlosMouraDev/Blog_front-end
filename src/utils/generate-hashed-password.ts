d0ee57cebff93e3a232e00dc9527b1dec4d0bf2e
Carlos Eduardo <cadumouralemes@gmail.com>
2025-09-03 17:23:01 -0300

    docs: adding comment on password hashed generator file

--- src/utils/generate-hashed-password.ts
+++ src/utils/generate-hashed-password.ts
@@ -1,7 +1,7 @@
 import { hashPassword } from '@/lib/login/password-hashing';
 
 (async () => {
-  const minhaSenha = ''; // DO NOT FORGET TO REMOVE YOUR PASSWORD LATER
+  const minhaSenha = ''; // DON'T FORGET TO REMOVE YOUR PASSWORD LATER
   const hashDaSuaSenhaEmBase64 = await hashPassword(minhaSenha);
 
   console.log({ hashDaSuaSenhaEmBase64 });
